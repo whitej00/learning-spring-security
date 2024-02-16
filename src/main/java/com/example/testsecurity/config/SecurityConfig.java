6af4b252544bcfe4733c30444cec27403d07db30
whitej00 <68831140+whitej00@users.noreply.github.com>
2024-02-16 20:36:08 +0900

    project load

---
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.testsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        http.formLogin((auth) -> auth.loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .permitAll()
+        );
+
+        /**
+         * csrf 적용시 form 전송에 csrf token이 필요함.
+         * 현재 개발 환경에서만 disable
+         * */
+        http.csrf((auth) -> auth.disable());
+
+        return http.build();
+    }
+}
